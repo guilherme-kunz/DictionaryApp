d377411345c931c05892e44aa02c99ca45b6e150
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2023-09-04 15:02:45 -0300

    First Commit

---
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/remote/dto/WordInfoDto.kt
@@ -0,0 +1,9 @@
+package com.plcoding.dictionary.feature_dictionary.data.remote.dto
+
+data class WordInfoDto(
+    val meanings: List<MeaningDto>,
+    val phonetic: String,
+    val phonetics: List<PhoneticDto>,
+    val sourceUrls: List<String>,
+    val word: String
+)
